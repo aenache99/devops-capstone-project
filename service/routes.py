af50b05f29f2a206515a9b19818a28410c0da370
Alex Enache <55555550+punmeistervstheworld@users.noreply.github.com>
2023-06-29 20:34:20 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -60,6 +60,8 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -128,7 +130,7 @@ def delete_accounts(account_id):
     account = Account.find(account_id)
     if account:
         account.delete()
-      
+    
     return "", status.HTTP_204_NO_CONTENT
 
 
